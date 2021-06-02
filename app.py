eff32e2cb04363382e6cbac664a137f85d8e4571
Lizzette Salmeron <lsalmer000@citymail.cuny.edu>
2021-06-02 15:13:06 -0400

    added hmtl text display to label inputs, added more inputs, changed Tilt and Yaw angle sliders to reflect float inputs

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 #Combining Tabs with callback and slider (input and slider not interacting)
+from os import path
 import dash
+# import dash_daq as daq
 import dash_html_components as html
 import dash_core_components as dcc
 
@@ -12,41 +14,79 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     dcc.Tabs(id='tabs-example', value='tab-1', children=[
         dcc.Tab(label='Geometry', children=[
-                #Hub Height
-                    dcc.Input(id="input-hubheight", type="number", min=0, max=20, value=3
+                #TSR
+                    html.P('Tip Speed Ratio:'),
+                    dcc.Input(id="input-TSR", type="number", min=0, max=20, placeholder='TSR input'
                     ),
-                    dcc.Slider(
+                #Blade Count
+                    html.P('Blade Count:'),
+                    html.Div(dcc.Slider(min=1, max=9, marks={i: '{}'.format(i) for i in range(10)}, value=1, 
+                    ), style={'width': '40%', 'padding': '0px 20px 20px 20px'}),
+                    
+                #Blade Pitch
+                    html.P('Blade Pitch:'),
+
+                #Generator Efficiency
+                    html.P('Generator Efficiency:'),
+                    
+                #Hub Height
+                    html.P('Hub Height:'),
+                    dcc.Input(id="input-hubheight", type="number", min=0, max=20, value=3 
+                    ),
+                    html.Div(dcc.Slider(
                         id="slider-hubheight", min=0, max=20, 
                         marks={i: str(i) for i in range(21)}, 
                         value=3
+                    ), style={'width': '40%', 'padding': '0px 20px 20px 20px'}),
+
+                #ngrid
+                    html.P('ngrid:'),
+
+                #pP
+                    html.P('pP:'),
+                #pT
+                    html.P('pT:'),
+                #rloc
+                    html.P('rloc:'),
+                #Tilt Angle
+                    html.P('Tilt Angle:'),
+                    dcc.Input(
+                        id="input-tiltang", type="number", min=0, max=20, value=0.0
                     ),
+                    html.Div(dcc.Slider(
+                        id="slider-tiltang", min=0, max=20, 
+                        marks={i: str(i) for i in range(21)}, 
+                        value=0.0,
+                        step=0.01,
+                        updatemode='drag'
+                    ), style={'width': '40%', 'padding': '0px 20px 20px 20px'}),
+
+                #Use points on perimeter
+                    html.P('Use points on perimeter:'),
+                #Yaw Angle
+                    html.P('Yaw Angle:'),
+                    dcc.Input(
+                        id="input-yawang", type="number", min=0, max=20, value=0.0
+                    ),
+                    html.Div(dcc.Slider(
+                        id="slider-yawang", min=0, max=20, 
+                        marks={i: str(i) for i in range(21)}, 
+                        value=0.0,
+                        step=0.01,
+                        updatemode='drag'
+                    ), style={'width': '40%', 'padding': '0px 20px 20px 20px'}),
+
                 #Rotor Diameter
+                    html.P('Rotor Diameter:'),
                     dcc.Input(
                         id="input-rotordiam", type="number", min=0, max=20, value=3
                     ),
-                    dcc.Slider(
+                    html.Div(dcc.Slider(
                         id="slider-rotordiam", min=0, max=20, 
                         marks={i: str(i) for i in range(21)}, 
                         value=3
-                    ),
-                #Tilt Angle
-                    dcc.Input(
-                        id="input-tiltang", type="number", min=0, max=20, value=3
-                    ),
-                    dcc.Slider(
-                        id="slider-tiltang", min=0, max=20, 
-                        marks={i: str(i) for i in range(21)}, 
-                        value=3
-                    ), 
-                #Yaw Angle
-                    dcc.Input(
-                        id="input-yawang", type="number", min=0, max=20, value=3
-                    ),
-                    dcc.Slider(
-                        id="slider-yawang", min=0, max=20, 
-                        marks={i: str(i) for i in range(21)}, 
-                        value=3
-                    ), 
+                    ), style={'width': '40%', 'padding': '0px 20px 20px 20px'}),
+                
         ]),
         dcc.Tab(label='Cp/Ct Table', children=[
             dcc.Graph(
@@ -63,28 +103,21 @@ app.layout = html.Div([
     ]),
 ])
 
+@app.callback(Output('input-TSR', 'value'),
+              Input('input-TSR', 'value'))
+def GeomTab_TSR_output(input_value):
+    print(input_value)
+    return input_value #fix this later
+
+
 @app.callback(Output('input-hubheight', 'value'),
               Output('slider-hubheight', 'value'),
                 Input('input-hubheight', 'value'),
-                Input('slider-hubheight', 'value'))
-            
-def GeomTab_hubheight_output(input_value1, slider_value1):
+                Input('slider-hubheight', 'value'))    
+def GeomTab_hubheight_output(input_value, slider_value):
             ctx = dash.callback_context
             trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
-            value1 = input_value1 if trigger_id == "input-hubheight" else slider_value1
-            print(input_value1, slider_value1, value1)
-            print(ctx.triggered)
-            return value1, value1
-
-@app.callback(Output('input-rotordiam', 'value'),
-              Output('slider-rotordiam', 'value'),
-                Input('input-rotordiam', 'value'),
-                Input('slider-rotordiam', 'value'))
-
-def GeomTab_rotordiam_output(input_value, slider_value):
-            ctx = dash.callback_context
-            trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
-            value = input_value if trigger_id == "input-rotordiam" else slider_value
+            value = input_value if trigger_id == "input-hubheight" else slider_value
             print(input_value, slider_value, value)
             print(ctx.triggered)
             return value, value
@@ -93,7 +126,6 @@ def GeomTab_rotordiam_output(input_value, slider_value):
               Output('slider-tiltang', 'value'),
                 Input('input-tiltang', 'value'),
                 Input('slider-tiltang', 'value'))
-
 def GeomTab_tiltang_output(input_value, slider_value):
             ctx = dash.callback_context
             trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
@@ -106,11 +138,22 @@ def GeomTab_tiltang_output(input_value, slider_value):
               Output('slider-yawang', 'value'),
                 Input('input-yawang', 'value'),
                 Input('slider-yawang', 'value'))
-
 def GeomTab_yawang_output(input_value, slider_value):
             ctx = dash.callback_context
             trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
             value = input_value if trigger_id == "input-yawang" else slider_value
+            print(input_value, slider_value, value)
+            print(ctx.triggered)
+            return value, value
+
+@app.callback(Output('input-rotordiam', 'value'),
+              Output('slider-rotordiam', 'value'),
+                Input('input-rotordiam', 'value'),
+                Input('slider-rotordiam', 'value'))
+def GeomTab_rotordiam_output(input_value, slider_value):
+            ctx = dash.callback_context
+            trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+            value = input_value if trigger_id == "input-rotordiam" else slider_value
             print(input_value, slider_value, value)
             print(ctx.triggered)
             return value, value
