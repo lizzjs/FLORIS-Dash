1fc2a7209aaa12cb41b02328cac1cd1e760c6c61
Lizzette Salmeron <lsalmer000@citymail.cuny.edu>
2021-06-10 14:29:50 -0400

    Added import JSON file functionality to home page

--- apps/model_builder/home.py
+++ apps/model_builder/home.py
@@ -5,14 +5,41 @@ import dash_html_components as html
 import dash_table
 
 import apps.model_builder.home_callbacks
+from pandas import DataFrame
 
-import_card = dbc.Card(
+import_spreadsheet_card = dbc.Card(
     dbc.CardBody(
         [
             html.H5("Import Spreadsheet", className="card-title"),
-            html.P("Import spreadsheet with turbine, farm, atomospheric conditions, and wake model parameters?"),
+            html.P("Import spreadsheet with turbine, farm, atomospheric conditions, and wake model parameters."),
             dcc.Upload(
                 id='home-upload-data', 
+                children=[dbc.Button("Select File", color="primary")],
+                style={
+                    # 'width': '20%',
+                    # 'height': '60px',
+                    'lineHeight': '60px',
+                    'borderWidth': '1px',
+                    'borderStyle': 'dashed',
+                    'borderRadius': '5px',
+                    'textAlign': 'center',
+                    # 'margin': '10px'
+                },
+                # Allow multiple files to be uploaded
+                multiple=True #change to true if you want multiple files 
+            ),
+            
+        ]
+    )
+)
+
+import_json_card = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H5("Import JSON file", className="card-title"),
+            html.P("Import JSON file with turbine, farm, atomospheric conditions, and wake model parameters."),
+            dcc.Upload(
+                id='json-upload-data', 
                 children=[dbc.Button("Select File", color="primary")],
                 style={
                     # 'width': '20%',
@@ -44,9 +71,39 @@ continue_card = dbc.Card(
     )
 )
 
-cards = dbc.Row([dbc.Col(import_card, width=4), dbc.Col(continue_card, width=8)])
+cards = dbc.Row(
+    [
+        dbc.Col( children=[
+            import_spreadsheet_card,
+            import_json_card], 
+            width=5
+        ),
+        dbc.Col(
+            continue_card, width=7
+        )
+    ]
+)
 
-layout = html.Div([
-            cards,
-            html.Div(id='editing-table-data-output'),
-        ])
+dummy_df = DataFrame({})
+table =dbc.Row(
+    [   
+        dash_table.DataTable(
+            id = 'homepage-datatable',
+            data=dummy_df.to_dict("rows"),
+            columns=[{"name": i, "id": i} for i in dummy_df.columns],
+            style_table={'height': '300px', 'overflowY': 'auto'},
+        )
+    ]
+)
+
+layout = dbc.Card(
+        dbc.CardBody(
+            [
+                html.Div([
+                    cards,
+                    table,
+                    html.Div(id='editing-table-data-output'),
+                ])
+            ]
+        )
+    )
--- apps/model_builder/home_callbacks.py
+++ apps/model_builder/home_callbacks.py
@@ -5,58 +5,44 @@ import plotly.graph_objs as go
 import io
 import pandas as pd
 
-import dash_table
 import base64
 import dash_html_components as html
 
 from app import app
 
 @app.callback(
-    Output('editing-table-data-output', 'children'),
+    [Output('homepage-datatable', 'data'),
+    Output('homepage-datatable', 'columns')],
     [Input('home-upload-data', 'contents'),
-    Input('home-upload-data', 'filename')]
+    Input('home-upload-data', 'filename')],
+    [Input('json-upload-data', 'contents'),
+    Input('json-upload-data', 'filename')]
 )
-def display_table(contents, filename):
-    #TODO move 'layout' contents to home.py? Tried but failed
-    #TODO check if edited cells are saved
-    table = html.Div()
+def display_table(contents, filename, jcontents, jfilename):
+    _module_df = pd.DataFrame({})
 
-    if contents:
+    if contents is not None:
         contents = contents[0]
         filename = filename[0]
-        df = parse_contents(contents, filename)
+        _module_df = parse_contents(contents, filename)
+    elif jcontents is not None:
+        contents = jcontents[0]
+        filename = jfilename[0]
+        _module_df = parse_contents(contents, filename)
 
-        table = html.Div(
-            [
-                html.H5(filename),
-                dash_table.DataTable(
-                    id = 'datatable-interactivity',
-                    data=df.to_dict("rows"),
-                    columns=[{"name": i, "id": i} for i in df.columns],
-                    editable=True,
-                    cell_selectable=True,
-                    column_selectable="multi",  # allow users to select 'multi' or 'single' columns
-                    row_selectable="multi",     # allow users to select 'multi' or 'single' rows
-                    row_deletable=True,         # choose if user can delete a row (True) or not (False)
-                    selected_columns=[],        # ids of columns that user selects
-                    selected_rows=[],           # indices of rows that user selects
-                    page_action="none",         # all data is passed to the table up-front or not ('none')
-                    style_table={'height': '300px', 'overflowY': 'auto'},
-                ),
-            ]
-        )
-        print(df)
-    return table
+    columns = [{"name": i, "id": i} for i in _module_df.columns]
+    return _module_df.to_dict("rows"), columns
 
 def parse_contents(contents, filename):
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string)
-    
+    print(decoded)
     if 'xls' in filename:
         # Assume that the user uploaded an excel file
         df = pd.read_excel(io.BytesIO(decoded), sheet_name='cpctws')
-        #TODO make separate dataframes for each sheet or do that in update_table method?
+    elif 'json' in filename:
+        df = pd.read_json(decoded)
     else:
-        pass
+        raise ValueError("The file imported was not in the expected file format.")
 
     return df
