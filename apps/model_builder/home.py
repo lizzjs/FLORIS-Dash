3d91eb70cd9cb9618f68c241a2ee0978e243ec9a
Rafael M Mudafort <rafmudaf@gmail.com>
2021-06-15 10:48:26 -0500

    Fix json upload parsing, load contents to Store

--- apps/model_builder/home.py
+++ apps/model_builder/home.py
@@ -2,72 +2,38 @@
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table
-
-from pandas import DataFrame
 
 
 import_json_card = dbc.Card(
-    dbc.CardBody(
-        [
-            html.H5("Import JSON file", className="card-title"),
-            html.P("Import JSON file with turbine, farm, atomospheric conditions, and wake model parameters."),
-            dcc.Upload(
-                id='json-upload-input-file', 
-                children=[dbc.Button("Select File", color="primary", id="jupload-button")],
-                style={
-                    # 'width': '20%',
-                    # 'height': '60px',
-                    'lineHeight': '60px',
-                    'borderWidth': '1px',
-                    'borderStyle': 'dashed',
-                    'borderRadius': '5px',
-                    'textAlign': 'center',
-                    # 'margin': '10px'
-                },
-                # Allow multiple files to be uploaded
-                multiple=True #change to true if you want multiple files 
-            ),
-            dbc.Spinner(html.Div(id="jloading-output")),
-        ]
-    )
+    dbc.CardBody([
+        html.H5("Import FLORIS input file", className="card-title"),
+        html.P("Select a JSON file with turbine, farm, atomospheric conditions, and wake model definitions."),
+        dcc.Upload(
+            id='json-upload-input-file', 
+            children=dbc.Button("Select File", color="primary", id="jupload-button"),
+            style={
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+            },
+        ),
+        dbc.Spinner(html.Div(id="jloading-output")),
+    ])
 )
 
 continue_card = dbc.Card(
-    dbc.CardBody(
-        [
-            html.H5("Getting started", className="card-title"),
-            html.P("Begin the Model Builder with no prepopulated data and enter all parameters directly."),
-            dbc.Button("Continue", color="primary", href="/build/windrose"),
-        ]
-    )
+    dbc.CardBody([
+        html.H5("Getting started", className="card-title"),
+        html.P("Begin the Model Builder with no prepopulated data and enter all parameters directly."),
+        dbc.Button("Continue", color="primary", href="/build/windrose"),
+    ])
 )
 
-cards = dbc.Row(
-    [
-        dbc.Col(import_json_card, width=5),
-        dbc.Col(continue_card, width=7)
-    ]
+layout = html.Div(
+    dbc.Row([
+        dbc.Col(import_json_card, width=6),
+        dbc.Col(continue_card, width=6)
+    ])
 )
-
-datatable=dbc.Row(
-    [   
-        dash_table.DataTable(
-            id = 'homepage-datatable',
-            data=[],
-            columns=[],
-            style_table={'height': '300px', 'overflowY': 'auto'},
-        )
-    ]
-)
-
-layout = dbc.Card(
-        dbc.CardBody(
-            [
-                html.Div([
-                    cards,
-                    # datatable,
-                ])
-            ]
-        )
-    )
--- apps/model_builder/home_callbacks.py
+++ apps/model_builder/home_callbacks.py
@@ -2,46 +2,26 @@
 import base64
 from dash.dependencies import Input, Output
 import dash_html_components as html
-import io
 import json
-import pandas as pd
 
 from app import app
-import apps.floris_data
-import time
 
 
-# @app.callback(
-#     Output('homepage-datatable', 'children'),
-#     [Input('json-upload-input-file', 'contents'),
-#     Input('json-upload-input-file', 'filename')]
-# )
-# def load_json_input_file(contents, filename):
-#     if contents is not None:
-#         try:
-#             # decoded = base64.b64decode(contents[0]).decode('utf-8', "replace")
-#             # print("decoded", decoded)
-#             # iostring = io.StringIO(decoded)
-#             # print("iostring", iostring)
-#             # data = json.loads(io.StringIO(decoded))
-#             # print("data", data)
-#             data = json.load( open("test_data/" + filename[0], 'r') )
-#             apps.floris_inputs.user_defined_dict = data
-#             return data
-#         except Exception as e:
-#             print(e)
-#             return html.Div(['There was an error processing this file.'])
-            
 @app.callback(
+    Output('floris-inputs', 'data'),
     Output("jloading-output", "children"),
-    [Input('json-upload-input-file', 'contents'),
-    Input('json-upload-input-file', 'filename')], 
-    Input("jupload-button", "n_clicks"),
+    Input('json-upload-input-file', 'contents'),
+    Input('json-upload-input-file', 'filename')
 )
-def load_output(contents, filename, n):
-    if n:
-        time.sleep(1)
-        return f"{filename} uploaded"
-    elif n==None:
-        return f"Upload file" 
-    return "Upload unsuccessful"
+def load_json_input_file(contents, filename):
+    if contents is None:
+        return {}, html.Div()
+
+    content_type, content_string = contents.split(',')
+    try:
+        decoded = base64.b64decode(content_string).decode('utf-8')
+        data = json.loads(decoded)
+        return data, html.Div([f'Successfully uploaded {filename}'])
+    except Exception as e:
+        print(e)
+        return {}, html.Div([f'There was an error processing {filename}: {e}.'])
