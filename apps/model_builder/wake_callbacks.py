1d8b2a34a88f61d6b9596aa1d982ea1e41c0ac5d
Rafael M Mudafort <rafmudaf@gmail.com>
2021-06-17 13:58:17 -0500

    Bug fix in the wake model selection collapse
    It was triggering the collapse when selecting a new wake model and n_clicks > 0.

--- apps/model_builder/wake_callbacks.py
+++ apps/model_builder/wake_callbacks.py
@@ -6,25 +6,6 @@ from app import app
 import apps.floris_data
 from floris.tools.floris_interface import FlorisInterface
 
-# @app.callback(Output('radio-deficit', 'value'), Input('radio-deficit', 'value'))
-# def velocity_deficit(value):
-#     apps.floris_data.user_defined_dict["wake"]["properties"]["velocity_model"] = value
-#     return apps.floris_data.user_defined_dict["wake"]["properties"]["velocity_model"]
-
-# @app.callback(Output('radio-deflection', 'value'), Input('radio-deflection', 'value'))
-# def deflection(value):
-#     apps.floris_data.user_defined_dict["wake"]["properties"]["deflection_model"] = value
-#     return apps.floris_data.user_defined_dict["wake"]["properties"]["deflection_model"]
-
-# @app.callback(Output('radio-turbulence', 'value'), Input('radio-turbulence', 'value'))
-# def turbulence(value):
-#     apps.floris_data.user_defined_dict["wake"]["properties"]["turbulence_model"] = value
-#     return apps.floris_data.user_defined_dict["wake"]["properties"]["turbulence_model"]
-
-# @app.callback(Output('radio-combination', 'value'), Input('radio-combination', 'value'))
-# def combination(value):
-#     apps.floris_data.user_defined_dict["wake"]["properties"]["combination_model"] = value
-#     return apps.floris_data.user_defined_dict["wake"]["properties"]["combination_model"]
 
 @app.callback(
     Output('radio-deficit', 'value'),
@@ -39,18 +20,17 @@ from floris.tools.floris_interface import FlorisInterface
     Input('radio-combination', 'value'),
     State("collapse-models", "is_open"),
 )
-def toggle_model(n, velocity_value, deflection_value, turbulence_value, combination_value, is_open,):
-
-    # print(is_open)
+def toggle_model(n, velocity_value, deflection_value, turbulence_value, combination_value, is_open):
 
     apps.floris_data.user_defined_dict["wake"]["properties"]["velocity_model"] = velocity_value
     apps.floris_data.user_defined_dict["wake"]["properties"]["deflection_model"] = deflection_value
     apps.floris_data.user_defined_dict["wake"]["properties"]["turbulence_model"] = turbulence_value
     apps.floris_data.user_defined_dict["wake"]["properties"]["combination_model"] = combination_value
 
-    if n:
-        # return not is_open, apps.floris_data.user_defined_dict["wake"]["properties"]["velocity_model"]
-        return velocity_value, deflection_value, turbulence_value, combination_value, not is_open
+    ctx = dash.callback_context
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    if trigger_id == "collapse-model-button":
+        is_open = not is_open
 
     return velocity_value, deflection_value, turbulence_value, combination_value, is_open
 
@@ -75,13 +55,6 @@ def toggle_parameters(n, velocity_value, deflection_value, turbulence_value, com
     fi = FlorisInterface(input_dict=apps.floris_data.default_input_dict)
     params = fi.get_model_parameters()
 
-    print(params)
-    
-    apps.floris_data.user_defined_dict["wake"]["properties"]["velocity_model"] = velocity_value
-    apps.floris_data.user_defined_dict["wake"]["properties"]["deflection_model"] = deflection_value
-    apps.floris_data.user_defined_dict["wake"]["properties"]["turbulence_model"] = turbulence_value
-    apps.floris_data.user_defined_dict["wake"]["properties"]["combination_model"] = combination_value
-
     vel_columns = [{"name": i, "id": i} for i in ["Parameter", "Value"]]
     vel_values = [ {"Parameter": k, "Value": v} for k, v in params["Wake Velocity Parameters"].items() ]
 
@@ -96,4 +69,4 @@ def toggle_parameters(n, velocity_value, deflection_value, turbulence_value, com
     if trigger_id == "collapse-parameter-button":
         is_open = not is_open
 
-    return vel_values, vel_columns, def_values, def_columns, turb_values, turb_columns, is_open # deflection_value, turbulence_value, combination_value, is_open
+    return vel_values, vel_columns, def_values, def_columns, turb_values, turb_columns, is_open
