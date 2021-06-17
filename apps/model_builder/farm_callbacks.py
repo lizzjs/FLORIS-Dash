06d3620b0501bc67a7c9685532363e9d031e894c
Rafael M Mudafort <rafmudaf@gmail.com>
2021-06-17 13:39:06 -0500

    Safely handle plotting the boundary line
    Avoids trying to access to boundary dataframe when it doesnt exist

--- apps/model_builder/farm_callbacks.py
+++ apps/model_builder/farm_callbacks.py
@@ -22,24 +22,28 @@ def create_farm_layout_plots(farm_data, n, boundary_data):
     ctx = dash.callback_context
     trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
 
+    figure_data = [
+        go.Scatter(
+            x=df['layout_x'],
+            y=df['layout_y'],
+            mode='markers'
+        )
+    ]
+
     if boundary_data is not None and trigger_id == "textarea-boundary-button":
         boundary_data = io.StringIO(boundary_data)
         df_b = pd.read_csv(boundary_data, sep=",")
         df2 = pd.DataFrame(df_b) #df_b does not have .append attribute
         df2 = df2.append(df2.iloc[0,:], ignore_index=True)
-
-    fig = go.Figure(
-        data=[
-            go.Scatter(
-                x=df['layout_x'],
-                y=df['layout_y'],
-                mode='markers'
-            ),
+        figure_data.append(
             go.Line(
                 x=df2['boundary_x'],
                 y=df2['boundary_y'],
             )
-        ],
+        )
+
+    fig = go.Figure(
+        data=figure_data,
         layout=go.Layout(
             plot_bgcolor=colors["graphBackground"],
             # paper_bgcolor=colors["graphBackground"]
