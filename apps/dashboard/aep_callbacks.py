93f9401490cc078fc2b928a04491aa29a8fc6fdc
Lizzette Salmeron <lsalmer000@citymail.cuny.edu>
2021-06-23 18:15:28 -0400

    Building on layout and callbacks files to display wind farm and windrose graphs

--- apps/dashboard/aep_callbacks.py
+++ apps/dashboard/aep_callbacks.py
@@ -3,6 +3,7 @@ from dash.dependencies import Input, Output, State
 from floris.simulation import floris
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objs as go
 import numpy as np
 
 from app import app
@@ -10,25 +11,27 @@ import apps.floris_data
 
 
 @app.callback(
-    Output('model-comparison-grpah', 'figure'),
+    # Output('model-comparison-grpah', 'figure'),
     Output('compute-time-graph', 'figure'),
+    Output('aep-farm-graph', 'figure'),
+    Output('aep-windrose-graph', 'figure'),
     Input("floris-outputs", "data")
 )
 def create_dashboard_plots(floris_output_data):
 
     # FLORIS AEP with and without wake steering
-    columns = ["Real AEP"], #"Ideal AEP", "Optimal AEP"]
-    model_name = apps.floris_data.default_input_dict["wake"]["properties"]["velocity_model"] #change this
-    values = [[model_name, floris_output_data[model_name]]]
-    df = pd.DataFrame(values, columns=columns)
+    # columns = ["Real AEP"] #, "Ideal AEP", "Optimal AEP"]
+    # model_name = apps.floris_data.default_input_dict["wake"]["properties"]["velocity_model"] #change this
+    # values = [[model_name, floris_output_data[model_name]]]
+    # df = pd.DataFrame(values, columns=columns)
 
-    fig1 = px.scatter(
-        df,
-        x=columns[0],
-        y=columns[1],
-        template="seaborn",
-        title="FLORIS AEP with and without Wake Steering"
-    )
+    # fig1 = px.scatter(
+    #     df,
+    #     x=columns[0],
+    #     y=columns[1],
+    #     template="seaborn",
+    #     title="FLORIS AEP with and without Wake Steering"
+    # )
 
     # Compute time
     columns_ct = ["Model Name", "Compute Time"]
@@ -42,5 +45,57 @@ def create_dashboard_plots(floris_output_data):
         template="seaborn",
         title="Compute Time"
     )
-    return fig1, fig2
+
+    #Wind farm
+    df_wf = pd.DataFrame(
+            {
+                'layout_x': apps.floris_data.user_defined_dict["farm"]["properties"]["layout_x"],
+                'layout_y': apps.floris_data.user_defined_dict["farm"]["properties"]["layout_y"]
+            }
+        )
+    figure_data = [
+        go.Scatter(
+            x=df_wf['layout_x'],
+            y=df_wf['layout_y'],
+            mode='markers'
+        )
+    ]
+    boundary_data = pd.DataFrame(
+            {
+                'boundary_x': apps.floris_data.farm_boundary["boundary_x"],
+                'boundary_y': apps.floris_data.farm_boundary["boundary_y"]
+            }
+        )
+    print("type: ", type(boundary_data), "contents: ")
+    print(boundary_data)
+    if boundary_data is not None:
+        df_bf = boundary_data.append(boundary_data.iloc[0,:], ignore_index=True)
+        figure_data.append(
+            go.Line(
+                x=df_bf['boundary_x'],
+                y=df_bf['boundary_y'],
+            )
+        )
+    fig4 = go.Figure(
+        data=figure_data,
+        layout=go.Layout(
+            # plot_bgcolor=colors["graphBackground"],
+            # paper_bgcolor=colors["graphBackground"]
+        )
+    )
+    
+
+    # Windrose
+    df = pd.DataFrame(apps.floris_data.wind_rose_data)
+    fig5 = px.bar_polar(
+        df,
+        r="frequency",
+        theta="direction",
+        color="strength",
+        template="seaborn",
+        color_discrete_sequence=px.colors.sequential.Plasma_r,
+        title="Wind Rose"
+    )
+
+    return fig2, fig4, fig5 #fig1, 
 
--- apps/dashboard/aep_layout.py
+++ apps/dashboard/aep_layout.py
@@ -5,8 +5,6 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
-# from apps.model_builder.atmos_cond_layout import wind_rose_graph
-# from apps.model_builder.farm_layout import farm_graph
 from apps.floris_connection.run_floris import calculate_wake
 import apps.floris_data
 
@@ -18,22 +16,23 @@ layout = dbc.Container(
     [
         html.Div(id="floris-compute-time", style={"hidden": "true"}),
         dbc.Row([
-            dbc.Col(id='model-comparison-grpah'),
+            dbc.Col(dcc.Graph(id='model-comparison-grpah')),
             dbc.Col(dcc.Graph(id='compute-time-graph'))
         ]),
      
-        dbc.Row(
+        dbc.Row([
             dbc.Col(
                 [
-                    dbc.Col(id='energy-gain-graph')
-                ]  
-            )
+                    dbc.Col(dcc.Graph(id='energy-gain-graph'))
+                ],
+           )
+        ], justify="center"
         ),
    
         dbc.Row(
             [
-                # dbc.Col([farm_graph,]),
-                # dbc.Col([wind_rose_graph,])
+                dbc.Col(dcc.Graph(id='aep-farm-graph')),
+                dbc.Col(dcc.Graph(id='aep-windrose-graph'))
             ], justify="around"
         )
     ],
