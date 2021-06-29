84259a91389ccd551986c6dc24d4d0c42fc80bac
Lizzette Salmeron <lsalmer000@citymail.cuny.edu>
2021-06-29 13:44:56 -0400

    Initial review page graph summary

--- apps/model_builder/review_callbacks.py
+++ apps/model_builder/review_callbacks.py
@@ -1,16 +1,57 @@
 
 from dash.dependencies import Input, Output, State
+import apps.model_builder.review_callbacks
 import numpy as np
+import pandas as pd
 import time
 
-from app import app
+from app import app, colors
 import apps.floris_data
 
+import floris.tools as ft
 from floris.tools.floris_interface import FlorisInterface
 from floris.tools.optimization.scipy.yaw_wind_rose import YawOptimizationWindRose
 import floris.tools.power_rose as pr
 import floris.tools.wind_rose as rose
 
+import plotly.graph_objs as go
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+# def get_floris_calc_cp_ct():
+#     # Initialize the FLORIS interface fi
+#     fi = ft.floris_interface.FlorisInterface(input_dict=apps.floris_data.default_input_dict)
+
+#     saved_layout_y = apps.floris_data.default_input_dict["farm"]["properties"]["layout_x"]
+#     saved_layout_x = apps.floris_data.default_input_dict["farm"]["properties"]["layout_y"]
+
+#     fi.reinitialize_flow_field(layout_array=([0], [0]))
+#     cp_return_array = []
+#     ct_return_array = []
+
+#     wind_speeds = apps.floris_data.default_input_dict["turbine"]["properties"]["power_thrust_table"]["wind_speed"] 
+
+#     for ws in wind_speeds:
+#         fi.reinitialize_flow_field(wind_speed=ws)
+#         fi.calculate_wake()
+#         ct_return_array.append(fi.get_turbine_ct()[0])
+
+#         area = np.pi*fi.floris.rotor_radius #probably not correct
+#         cp = fi.get_turbine_power()[0]* area**3 #conversion for cp, missing stuff
+#         ct_return_array.append(cp)
+
+#     fi.reinitialize_flow_field(layout_array=(saved_layout_x, saved_layout_y))
+
+#     ct_return_array = np.array(ct_return_array)
+#     cp_return_array = np.array(ct_return_array)
+#     wind_speed = np.array(wind_speeds)
+
+#     coeff_dict = {'Wind Speed':wind_speed, 'CpU':cp_return_array, 'CtU':ct_return_array}
+#     df = pd.DataFrame(coeff_dict)
+
+#     plt.plot(df['Wind Speed'], df["CpU"])
+#     plt.show()
+#     return df
 
 @app.callback(
     # Output("loading-output", "children"),
@@ -83,3 +124,65 @@ def run_floris(n, floris_output_data):
         }
     }
     return floris_output_data
+
+@app.callback(
+    Output('review-windrose-graph', 'figure'),
+    Output('review-wind-farm-layout', 'figure'),
+    # Output('aep-farm-graph', 'figure'),
+    # Output('aep-windrose-graph', 'figure'),
+    Input("floris-outputs", "data")
+)
+def return_review_page_graphs(floris_output_data):
+    #Windrose
+    wind_rose_figure = px.bar_polar(
+        pd.DataFrame(apps.floris_data.wind_rose_data),
+        r="frequency",
+        theta="direction",
+        color="strength",
+        template="seaborn",
+        color_discrete_sequence=px.colors.sequential.Plasma_r,
+        title="Wind Rose"
+    )
+
+    #Wind farm 
+    layout_data = pd.DataFrame(
+        {
+            'layout_x': apps.floris_data.user_defined_dict["farm"]["properties"]["layout_x"],
+            'layout_y': apps.floris_data.user_defined_dict["farm"]["properties"]["layout_y"]
+        }
+    )
+    boundary_data = pd.DataFrame(
+        {
+            'boundary_x': apps.floris_data.boundary_data["boundary_x"],
+            'boundary_y': apps.floris_data.boundary_data["boundary_y"]
+        }
+    )
+
+    layout_plot_data = [
+        go.Scatter(
+            x=layout_data['layout_x'],
+            y=layout_data['layout_y'],
+            mode='markers'
+        )
+    ]
+
+    if boundary_data is not None:
+        df_bf = boundary_data.append(boundary_data.iloc[0,:], ignore_index=True)
+        layout_plot_data.append(
+            go.Line(
+                x=df_bf['boundary_x'],
+                y=df_bf['boundary_y'],
+            )
+        )
+    wind_farm_figure = go.Figure(
+        data=layout_plot_data,
+        layout=go.Layout(
+            # plot_bgcolor=colors["graphBackground"],
+            # paper_bgcolor=colors["graphBackground"]
+        )
+    )
+
+    #Cp Ct 
+    # df_floris_calc = get_floris_calc_cp_ct()
+
+    return wind_rose_figure, wind_farm_figure
--- apps/model_builder/review_layout.py
+++ apps/model_builder/review_layout.py
@@ -1,5 +1,6 @@
 
 import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 import dash_html_components as html
 import json
 
@@ -21,14 +22,30 @@ display_json = html.Div(
 submit_button = html.Div([
     dbc.ListGroupItem(
         dbc.Button("Submit", id="submit-floris-button", block=True, color="primary", href="/aep-results"),
-    )
-    # dbc.Spinner(html.Div(id="loading-output")),
+    ),
+    dbc.Spinner(html.Div(id="loading-output")),
 ])
 
 layout = dbc.Container([
-    dbc.Card(
-        dbc.CardBody(display_json),
-        style={"maxHeight": "550px","overflow": "scroll"},
-    ),
-    submit_button,
+    dbc.Row([
+        dbc.Col(
+            [
+                dbc.Card(
+                    dbc.CardBody(display_json),
+                    style={"maxHeight": "1831px","overflow": "scroll"},
+                ),
+                submit_button,
+            ], width=4
+        ),
+        dbc.Col([
+            dbc.Card(
+                dbc.CardBody([
+                    dcc.Graph(id='review-windrose-graph'),
+                    dcc.Graph(id='review-wind-farm-layout'),
+                    dcc.Graph(id='review-cp-comparison-graph'),
+                    dcc.Graph(id='review-ct-comparison-graph'),
+                ]),
+            ),
+        ])
+    ])
 ])
